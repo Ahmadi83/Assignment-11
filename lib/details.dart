497c4963f797312f7d732fc4b27163d9c9334531
AHMADI <abdulrazeqahmaddi@gmail.com>
2024-08-15 21:57:15 +0430

    third commit

--- lib/details.dart
+++ lib/details.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 
 
 class Details extends StatelessWidget {
@@ -7,6 +9,7 @@ class Details extends StatelessWidget {
   final String picture;
   final String description;
   final String country;
+  final url='https://www.google.com/';
 
   const  Details({super.key, required this.name, required this.picture, required this.description, required this.country});
 
@@ -14,20 +17,39 @@ class Details extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(children:[
-        Padding(
-          padding: const EdgeInsets.only(bottom: 20),
-          child: Center(child: Text(name,style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),)),
-        ),
-        Center(child:CircleAvatar(
-              backgroundImage: AssetImage(picture),
-              radius: 100,
-            )),
-            SizedBox(height: 10,),
+      body: ListView(
+        children: [Column(children:[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Center(child: Text(name,style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold),)),
+          ),
+          Center(child:CircleAvatar(
+                backgroundImage: AssetImage(picture),
+                radius: 100,
+              )),
+              SizedBox(height: 10,),
 
-        Container(width: 480,height: 1,color: Colors.black54,),
-        Center(child: Text(description))
-      ],)
+          Container(width: 480,height: 1,color: Colors.black54,),
+
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(width: double.infinity,height:1700,color: Colors.white,child:
+                Column(mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                  Text("$name",style: TextStyle(fontSize: 30,
+                  ),),
+                  Text(description,)
+                ],)
+              ),
+          ),
+
+
+          ElevatedButton(onPressed: () async{
+            final linkToOpen =Uri.parse(url);
+            await launchUrl(linkToOpen, );
+            }, child: Text("More"))
+        ],),
+      ])
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 
 void main(){
 
- runApp(MaterialApp(debugShowCheckedModeBanner: true,
+ runApp(MaterialApp(debugShowCheckedModeBanner: false,
    home: MyApp(),
  )) ;
 }
@@ -23,64 +23,82 @@ class _MyAppState extends State<MyApp> {
 
   List<Map<String,dynamic>> list =[
     {
-    'name': 'Shaikh Abdul Basit-Abdul Samad',
+    'name': 'شیخ عبدالباسط',
       'picture': 'images/basit.jpg',
       'description': "Abdul rahman his last name is al Sodais",
-      'country': 'Egypt'
+      'country': 'مصر'
   },
     {
-      'name': 'Shaikh Mahmood Al Khalil Al-Hussary',
+      'name': 'شیخ محمود الخلیل ',
       'picture': 'images/mah.jpg',
       'description': "Abdul rahman his last name is al Sodais",
-      'country': 'Egypt'
+      'country': 'مصر'
     },
     {
-      'name': 'Shaikh Mohammad Sidiq -Minshawi',
-      'picture': 'images/basit.jpg',
+      'name': ' شیخ سعد الغامدی ',
+      'picture': 'images/ghamedi.jpg',
       'description': "Abdul rahman his last name is al Sodais",
-      'country': 'Egypt'
+      'country': 'عربستان سعودی'
     },
     {
-      'name': 'Shaikh Maher Al-Muiaiqly',
-      'picture': 'images/basit.jpg',
+      'name': 'شیخ ماهر المعیقلی',
+      'picture': 'images/maher1.jpg',
       'description': "Abdul rahman his last name is al Sodais",
-      'country': 'Egypt'
+      'country': 'عربستان سعودی'
     },
     {
-      'name': 'Shaikh Abdul Rahman Al-Sudais',
+      'name': 'شیخ عبدالرحمن السدیس',
       'picture': 'images/Asodais.jpg',
-      'description': "Abdul rahman his last name is al Sodais",
-      'country': 'Saudi Arabia'
+      'description': '''
+       عبدالرحمن السدیس، در سال ۱۹۶۰ در ریاض، عربستان سعودی به دنیا آمد و امام جماعت مقدس‌ترین مسجد مسلمانان در مکه، یعنی مسجد الحرام، می‌باشد.
+        او یک قاری قرآن به روش حفظ است. صدای او در میان تلاوت کنندگان قرآن شناخته شده و معروف است. هم‌اکنون رئیس کمیته مدیریت دو مسجد مقدس (مکه و مدینه) است
+      ابوعبدالعزیز عبدالرحمن بن عبدالعزیز بن عبدالله بن محمد بن عبدالعزیز بن محمد بن عبدالله السدیس، نسب وی به قبیله عنزه یکی از قبایل مشهور بکیره از مناطق قصیم می‌باشد. در سال ۱۹۶۰ در شهر ریاض پایتخت کشور عربستان سعودی دیده به جهان گشود. و در سن 12 سالگی با نظارت شیخ عبدالرحمن آل فریان موفق به حفظ کامل قرآن کریم گردید، و آخرین کسی که در نزدشان قرآن را خواندند شیخ محمد علی حسان بودند. وی در دبستان ابن حارث و سپس در مؤسسه علوم ریاض تحصیل کرد، جایی که در سال ۱۹۷۹ با درجه عالی فارغ‌التحصیل شد. بعداً به آکادمی اسلامی ریاض پیوست و در سال ۱۹۸۲ با مدرک شریعت فارغ‌التحصیل شد. سال ۱۹۸۷ کارشناسی ارشد را در رشته شریعت اسلامی در دانشگاه اسلامی امام محمد ابن سعود گذراند و دکتری خود را از دانشگاه ام‌القری در سال ۱۹۹۵ گرفت. در زمان گرفتن دکتری به عنوان استاد در آنجا مشغول تدریس بو
+      فعالیت‌ها:
+      پس فارغ التحصیل شدن از بخش اصول فقه به‌عنوان استادیار دانشکده شریعت انتخاب شدند.
+به‌عنوان امام و خطیب در چند مسجد شهر ریاض فعالیت نمودن که آخرین آنها مسجد جامع شیخ عبدالرزاق عفیفی رحمه الله بود.
+در سال ۱۴۰۴ هـ ق طی حکمی ایشان به‌عنوان امام و خطیب مسجدالحرام تعیین شدند.
+در سال ۱۴۰۸ هـ ق موفق به اخذ گواهی فوق لیسانس از دانشگاه امام محمد بن سعود با درجه ممتاز گشتند که استادان مشاور ایشان در نوشتن پایان نامه، شیخ عبدالرزاق عفیفی رحمه الله و دکتر عبدالرحمن الدویش بودند.
+استاد بخش دادگستری و قضاوت دانشکده شریعت دانشگاه ام القری مکه انتخاب شدند.
+شیخ عبدالرحمن السدیس در سال ۱۴۱۶ هـ ق موفق به اخذ گواهی دکتری از دانشکده شریعت دانشگاه ام القری مکه گردید که استاد مشاور ایشان در نگاشتن پایان نامه، شیخ احمد فهمی ابوسنه بودند.
+استادیار دانشکده شریعت دانشگاه ام القری مشغول به فعالیت شدند که همزمان با آن به امامت و خطابت در مسجد الحرام می پردازد.
+وی عضو چندین هیئت و مؤسسه علمی و دعوی و خیری می‌باشد و نیز حضور پررنگی در رسانه‌های جمعی داخلی و خارجی دارد، همچنین دارای اهتمام ویژه‌ای به مسائل علمی از طریق تدریس و ارائه بحث‌های علمی و تحقیقات مختلف می‌باشد.
+دیدگاه‌ها
+السدیس در مصاحبه با شبکه خبر سعودی طی سفر خود در آمریکا گفته بود:
+کشورش به رهبری ملک سلمان و آمریکا به ریاست دونالد ترامپ به عنوان دو قطب تأثیرگذار جهان و انسانیت را به سوی سواحل امنیت، ثبات، صلح و آرامش هدایت می‌کنند. این اظهار نظر از سوی چندین شخصیت شناخته شده و کاربران فعال شبکه‌های اجتماعی مورد انتقاد قرار گرفت. عبدالله شمری این سخنان السدیس در مورد آمریکا را مورد سخره گرفت و گفت بزرگترین دلیل این سخن کاری است که آمریکا در عراق و افغانستان از قبیل کشتار، تخریب و تعرض انجام داد و حمایت از صهیونیست‌ها در اشغال مقدسات مسلمانان. ترکی شلهوب یک ویدیو از جرایم نیروهای آمریکایی در عراق منتشر کرد که در زیرنویس آن نوشته شده بود که این همان دولتی است که السدیس می‌گوید عالم را به سوی صلح رهبری می‌کند. تاجر السر عثمان سخنان السدیس را در مورد آمریکا چنین توصیف کرد: «... تلاش کاهنانه برای پاک کردن پرونده سیاه آمریکا در عراق، افغانستان و فلسطین»
+
+رئیس کل اداره امور حرمین شریفین با بیان اینکه آنچه در غزه رخ می‌دهد یک فتنه است گفت: مسلمانان نباید به کاری که حق ندارند وارد آن شوند.
+      ''',
+      'country': 'عربستان سعودی'
     },
     {
-      'name': 'Shaikh Mishary Rashid Al-Aafasy',
-      'picture': 'images/basit.jpg',
+      'name': 'شیخ راشد العفاسی',
+      'picture': 'images/Afasi.jpg',
       'description': "Abdul rahman his last name is al Sodais",
-      'country': 'Egypt'
+      'country': 'کویت'
     },
     {
-      'name': 'Shaikh Ahmad Al-Ajmi',
-      'picture': 'images/basit.jpg',
+      'name': 'شیخ احمد العجمی',
+      'picture': 'images/ajami.jpg',
       'description': "Abdul rahman his last name is al Sodais",
-      'country': 'Egypt'
+      'country': 'عربستان سعودی'
     },
     {
-      'name': 'Shaikh Mohammad Siddiq',
-      'picture': 'images/basit.jpg',
+      'name': 'شیخ مصطفی اسماعیل',
+      'picture': 'images/esmaeil.jpg',
       'description': "Abdul rahman his last name is al Sodais",
-      'country': 'Egypt'
+      'country': 'مصر'
     },
     {
-      'name': 'Shaikh Abdul Samad ',
-      'picture': 'images/basit.jpg',
+      'name': 'شیخ طارق جمیل ',
+      'picture': 'images/tareq.jpg',
       'description': "Abdul rahman his last name is al Sodais",
-      'country': 'Egypt'
+      'country': 'پاکستان'
     },
     {
-      'name': 'Shaikh Salim Al-Hilali',
-      'picture': 'images/basit.jpg',
+      'name': 'شیخ سلیم الهلالی',
+      'picture': 'images/salim.jpg',
       'description': "Abdul rahman his last name is al Sodais",
-      'country': 'Egypt'
+      'country': 'یمن'
     },
   ];
 
@@ -91,8 +109,10 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey
+      ,
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.green,
         title: Text("Top 10 Quran's Readers ",
         style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
         actions:[PopupMenuButton(onSelected: (values){
@@ -111,10 +131,10 @@ class _MyAppState extends State<MyApp> {
           itemBuilder: (context, index) {
           return Card(
             child: ListTile(
-              shape: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+              shape: OutlineInputBorder(borderRadius: BorderRadius.circular(14)),
               splashColor: Colors.blueGrey,
-              title: Text(list[index]['name'],style: TextStyle(color: Colors.purple,fontSize: 18),),
-              subtitle: Text(list[index]['country'],style: TextStyle(color: Colors.red,fontSize: 17),),
+              title: Text(list[index]['name'],style: TextStyle(color: Colors.purple,fontSize: 20,fontWeight: FontWeight.w600),),
+              subtitle: Text(list[index]['country'],style: TextStyle(color: Colors.red,fontSize: 17,fontWeight: FontWeight.bold),),
               leading: CircleAvatar(
                 radius: 30,
                   backgroundImage: AssetImage(
